a9a6785aa1e5907a394080acb9311ec9ea5640be
Hallvard Jensen <hallvard.jensen@gmail.com>
2022-02-26 01:08:31 +0100

    restructuring entire project, removing python code, spring boot is now backend

---
+++ backend/src/main/java/com/github/ewanme/romresbot/RomresbotController.java
@@ -0,0 +1,23 @@
+package com.github.ewanme.romresbot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/romres")
+public class RomresbotController {
+	
+	private final RomresbotService service;
+	
+	@Autowired
+	public RomresbotController(RomresbotService service) {
+		this.service = new RomresbotService();
+	}
+	
+	@GetMapping
+	public String login() {
+		return service.reserveRoom();
+	}
+}
